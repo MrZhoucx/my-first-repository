32cc9c095657936766bbdb58f40eb71d49b4d6c2
周成兴 <1356152721@qq.com>
2020-04-11 18:04:46 +0800

    first time

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main( )
+{
+	cout<<"HelloWorld!!!"<<endl;
+	cout<<"2020.04.11"<<endl;
+	cout<<"The first time I upload my program on github."<<endl;
+	system("pause");
+	return 0; 
+ } 
